fdc98526dd96a190a4f46186125bcfa8e26eae37
Sean Rooney <seandanielrooney@gmail.com>
2020-04-10 16:35:31 -0700

    was able to have component for projects funtioning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,58 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import Projects from './components/projects'
+
+const projectList = [
+  {
+    name: "Password Generator",
+    technologies: "Javascript",
+    GitHub: "https://github.com/Sean-93/random-password-generator",
+    Heroku: ""
+
+  },
+  {
+    name: "Weather Dashboard",
+    technologies: "Javascript, JQuery",
+    GitHub: "https://github.com/Sean-93/weather-dashboard" ,
+    Heroku: ""
+
+  },
+  {
+    name: "Find Tune",
+    technologies: "Javascript",
+    GitHub: "https://github.com/justinkellogg89/find-tune" ,
+    Heroku: ""
+
+  },
+  {
+    name: "Node Readme Generator",
+    technologies: "Node.JS",
+    GitHub: " https://github.com/Sean-93/node-readme-generator" ,
+    Heroku: ""
+
+  },
+  {
+    name: "MySQL Employee Tracker",
+    technologies: "Node.JS, MySQL",
+    GitHub: " https://github.com/Sean-93/mysql-employee-tracker" ,
+    Heroku: ""
+
+  },
+  {
+    name: "Node Express Burger Tracker",
+    technologies: "Node, Express, Handlebars",
+    GitHub: "https://github.com/Sean-93/node-express-burger-log" ,
+    Heroku: "https://murmuring-chamber-35666.herokuapp.com/"
+
+  },
+  {
+    name: "Checkit",
+    technologies: "React, MongoDB, Mongoose",
+    GitHub: "https://github.com/Sean-93/checkit" ,
+    Heroku: ""
+
+  },
+]
 
 function App() {
   return (
@@ -8,6 +60,8 @@ function App() {
       <header className="App-header">
         
       </header>
+
+      <Projects projectList={projectList}/>
     </div>
   );
 }
---
+++ src/components/projects.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+
+export default function Projects(props) {
+  // const [employeesList, setEmployeesList] = useState(props.employeesList);
+  const employee = props.projectList.map((item, index) => {
+      return (
+        <tr>
+          <th scope="row">{index+1}</th>
+          <td>{item.name}</td>
+          <td>{item.technologies}</td>
+          <td>{item.GitHub}</td>
+          <td>{item.Heroku}</td>
+        </tr>
+      );
+    });
+    
+  function ActionLink (key) {
+    props.ActionLink(key);
+  }
+console.log("employee Render");
+  return (
+    <div>
+      <div className="container-fluid">
+        <h1>Highlighted Projects:</h1>
+        <table className="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name:</th>
+              <th scope="col">Technologies:</th>
+              <th scope="col">GitHub Link:</th>
+              <th scope="col">Heroku Link:</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+          {employee}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
