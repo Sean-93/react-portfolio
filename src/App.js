782b2d0b3bf9ddbd7f7f579cae804b96046d4c7b
Sean Rooney <seandanielrooney@gmail.com>
2020-04-10 18:45:57 -0700

    adding to Heroku

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import './App.css';
 import Projects from './components/projects'
 
 const projectList = [
@@ -8,49 +7,49 @@ const projectList = [
     name: "Password Generator",
     technologies: "Javascript",
     GitHub: "https://github.com/Sean-93/random-password-generator",
-    Heroku: ""
+    Application: "https://sean-93.github.io/random-password-generator"
 
   },
   {
     name: "Weather Dashboard",
     technologies: "Javascript, JQuery",
     GitHub: "https://github.com/Sean-93/weather-dashboard" ,
-    Heroku: ""
+    Application: "https://sean-93.github.io/weather-dashboard"
 
   },
   {
     name: "Find Tune",
     technologies: "Javascript",
     GitHub: "https://github.com/justinkellogg89/find-tune" ,
-    Heroku: ""
+    Application: "https://justinkellogg89.github.io/find-tune/"
 
   },
   {
     name: "Node Readme Generator",
     technologies: "Node.JS",
     GitHub: " https://github.com/Sean-93/node-readme-generator" ,
-    Heroku: ""
+    Application: "video: https://drive.google.com/file/d/1pH7W_hQB0Ij2Ij6LrLVPv9ZkMrBVCcad/view?usp=sharing"
 
   },
   {
     name: "MySQL Employee Tracker",
     technologies: "Node.JS, MySQL",
     GitHub: " https://github.com/Sean-93/mysql-employee-tracker" ,
-    Heroku: ""
+    Application: "video: https://drive.google.com/file/d/1ZbBHbeEWxy-31YYpLBlV0Xy1x3rceczR/view?usp=sharing"
 
   },
   {
     name: "Node Express Burger Tracker",
     technologies: "Node, Express, Handlebars",
     GitHub: "https://github.com/Sean-93/node-express-burger-log" ,
-    Heroku: "https://murmuring-chamber-35666.herokuapp.com/"
+    Application: "https://murmuring-chamber-35666.herokuapp.com/"
 
   },
   {
     name: "Checkit",
     technologies: "React, MongoDB, Mongoose",
     GitHub: "https://github.com/Sean-93/checkit" ,
-    Heroku: ""
+    Application: "In development"
 
   },
 ]
