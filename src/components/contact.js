9273d348805e0c0d12516b53dd0c51f03e90c744
Sean Rooney <seandanielrooney@gmail.com>
2020-04-10 20:51:13 -0700

    contact info

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,19 +4,21 @@ export default function Contact(props) {
   // const [employeesList, setEmployeesList] = useState(props.employeesList);
   const contact = props.contactInfo.map((item, index) => {
       return (
-        <div class="card" style="width: 18rem;">
-        <div class="card-header">
-             Contact Info:
-        </div>
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item">{item.fullName}</li>
-        <li class="list-group-item">{item.title}</li>
-        <li class="list-group-item">{item.email}</li>
-        <li class="list-group-item">{item.linkedIn}</li>
-        <li class="list-group-item">{item.gitHub}</li>
-        <li class="list-group-item">{item.phoneNumber}</li>
-        </ul>
-        </div>
+        <tr>
+          <td>{item.fullName}</td>
+          <br/>
+          <td>{item.title}</td>
+          <br/>
+          <td>{item.email}</td>
+          <br/>
+          <td>{item.linkedIn}</td>
+          <br/>
+          <td>{item.gitHub}</td>
+          <br/>
+          <td>{item.phoneNumber}</td>
+          <br/>
+        </tr>
+        
       );
     });
     
