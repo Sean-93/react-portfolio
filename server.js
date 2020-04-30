63266ec812339337850e94285a301727eb13f3fb
Sean Rooney <seandanielrooney@gmail.com>
2020-04-30 16:46:43 -0700

    troubleshooting heroku

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 const express = require('express');
 // const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const app = express();
 // app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'assets'))); //new, not sure if correct
+app.use(express.static(path.join(__dirname, 'static'))); //new, not sure if correct
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
