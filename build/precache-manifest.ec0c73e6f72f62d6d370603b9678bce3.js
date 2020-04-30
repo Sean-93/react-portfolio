1444f429745ecaa32d49164baaa7232cce19f4a9
Sean Rooney <seandanielrooney@gmail.com>
2020-04-30 15:21:11 -0700

    build for heroku

---
+++ build/precache-manifest.ec0c73e6f72f62d6d370603b9678bce3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d188b6654e80f84b1ecaa10f02376a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "936ebe2c03601a589559",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "03679d7f7601143e1d44",
+    "url": "/static/js/2.bf46046b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf46046b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936ebe2c03601a589559",
+    "url": "/static/js/main.fd8e39aa.chunk.js"
+  },
+  {
+    "revision": "a0ac1140309c735bc60b",
+    "url": "/static/js/runtime-main.6be4d7d3.js"
+  }
+]);
