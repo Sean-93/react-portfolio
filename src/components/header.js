929569507b1b7338bc1a8320296820353b2e0d56
Sean Rooney <seandanielrooney@gmail.com>
2020-05-01 00:12:45 -0700

    adding more to header and cleaning things up

--- src/components/header.js
+++ src/components/header.js
@@ -1,44 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Header(props) {
+  
   const header = props.headerInfo.map((item, index) => {
-    return (
-      <div className="container">
-        <h1 className="display-4 text-center">{item.fullName}</h1>
-        <p className="lead text-center">
-          {item.title} | {item.email}
-        </p>
-        <p className="lead text-center">
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={item.contactLink}
-            >
-              {item.contactLink} |{" "}
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={item.projectsLink}
-            >
-              {item.projectsLink} |{" "}
-            </a>
-            <a target="_blank" rel="noopener noreferrer" href={item.bioLink}>
-              {item.bioLink}
-            </a>
-          </td>
-        </p>
-      </div>
-    );
-  });
+      return (
+        
+    <div className="container">
+    <h1 className="display-4 text-center">{item.fullName}</h1>
+    <p className="lead text-center">{item.title} | {item.email}</p>
+    <p className="button">
+
+    <Link to="/contact">Contact</Link>{" | "}
+    <Link to="/projects">Projects</Link>{" | "}
+    <Link to="/bio">Main</Link>
+
+    </p>
+    </div>
+
+      );
+    });
+    
+    
+
+
 
   return (
     <div>
       <div className="jumbotron jumbotron-fluid">
-        <thead className="container text-center">{header}</thead>
-        <tbody></tbody>
+        
+          <thead className="container text-center">
+           
+          {header}
+
+          </thead>
+          <tbody>
+          </tbody>
+        
       </div>
     </div>
   );
