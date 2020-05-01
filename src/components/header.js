b4be69db62647461ba13e2c169df7e8e5cccb7db
Sean Rooney <seandanielrooney@gmail.com>
2020-05-01 00:23:33 -0700

    adding more to header and cleaning things up

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export default function Header(props) {
   
@@ -9,27 +9,17 @@ export default function Header(props) {
     <div className="container">
     <h1 className="display-4 text-center">{item.fullName}</h1>
     <p className="lead text-center">{item.title} | {item.email}</p>
-    <p className="button">
-
-    <Link to="/contact">Contact</Link>{" | "}
-    <Link to="/projects">Projects</Link>{" | "}
-    <Link to="/bio">Main</Link>
-
-    </p>
     </div>
 
       );
     });
     
-    
-
-
 
   return (
     <div>
-      <div className="jumbotron jumbotron-fluid">
+      <div className="jumbotron jumbotron-fluid text-center">
         
-          <thead className="container text-center">
+          <thead>
            
           {header}
 
